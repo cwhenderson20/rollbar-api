c82c01bf31301853b670336ef1515b133194ed66
Chris Henderson <chris@app-press.com>
2015-08-17 00:37:21 -0400

    More project specific methods and tests

--- test/rollbar-test.js
+++ test/rollbar-test.js
@@ -2,6 +2,7 @@ var request = require("request");
 var nock = require("nock");
 var assert = require("chai").assert;
 var Rollbar = require("../lib/rollbar");
+var ApiError = require("../lib/api-error");
 
 describe("Rollbar API", function () {
 	describe("Instantiation", function () {
@@ -104,6 +105,91 @@ describe("Rollbar API", function () {
 				assert.isNull(err);
 				assert.strictEqual(res.statusCode, 200);
 				assert.deepEqual(res.body, EXAMPLE_PROJ);
+			});
+		});
+
+		// ==> Handles errors when project doesn't exist
+
+		it("deletes a project", function () {
+			nock(BASE_URL)
+				.delete("/project/1234")
+				.query(true)
+				.reply(200, {
+					err: 0
+				});
+
+			api.deleteProject(1234, function (err, res) {
+				assert.isNull(err);
+				assert.strictEqual(res.statusCode, 200);
+				assert.isUndefined(res.body);
+			});
+		});
+
+		// Doesn't work right now... looking into why
+		// it("responds with an error when the project to delete does not exist", function () {
+		// 	nock(BASE_URL)
+		// 		.delete("/project/1234")
+		// 		.query(true)
+		// 		.reply(422, {
+		// 			err: 1,
+		// 			message: "Invalid project"
+		// 		});
+
+		// 	api.deleteProject(1234, function (err, res) {
+		// 		assert.isNotNull(err);
+		// 	});
+		// });
+
+		it("creates a project", function () {
+			nock(BASE_URL)
+				.post("/projects", {
+					name: "Sample-Project"
+				})
+				.query(true)
+				.reply(200, {
+					err: 0,
+					result: EXAMPLE_PROJ
+				});
+
+			api.createProject("Sample-Project", function (err, res) {
+				assert.isNull(err);
+				assert.strictEqual(res.statusCode, 200);
+				assert.deepEqual(res.body, EXAMPLE_PROJ);
+			});
+		});
+
+		it("list project access tokens", function () {
+			var tokenList = [{
+				project_id: 1234,
+				access_token: "abc123",
+				name: "write",
+				status: "enabled",
+				scopes: [
+					"write"
+				]
+			}, {
+				project_id: 1234,
+				access_token: "abc123",
+				name: "read",
+				status: "enabled",
+				scopes: [
+					"read"
+				]
+			}];
+
+			nock(BASE_URL)
+				.get("/project/1234/access_tokens")
+				.query(true)
+				.reply(200, {
+					err: 0,
+					result: tokenList
+				});
+
+			api.listProjectAccessTokens(1234, function (err, res) {
+				assert.isNull(err);
+				assert.strictEqual(res.statusCode, 200);
+				assert.isObject(res);
+				assert.deepEqual(res.body, tokenList);
 			});
 		});
 	});
